11fcbb37ab331c5aa86ff44166f1dd31f8498066
Arne Peters <arne.peters@tum.de>
2022-03-10 16:09:11 +0100

    Reverted 9f77d19efbef9c98393a2fac0e8f35ab2e020d73. Turned out the reader was broken.
    This reverts commit 9f77d19efbef9c98393a2fac0e8f35ab2e020d73.

--- src/k4a_ros_device.cpp
+++ src/k4a_ros_device.cpp
@@ -666,9 +666,8 @@ k4a_result_t K4AROSDevice::getPointCloud(const k4a::capture& capture, sensor_msg
 k4a_result_t K4AROSDevice::fillColorPointCloud(const k4a::image& pointcloud_image, const k4a::image& color_image,
                                                sensor_msgs::PointCloud2Ptr& point_cloud)
 {
-  // Flip height and width to match expected memory layout
-  point_cloud->height = pointcloud_image.get_width_pixels();
-  point_cloud->width = pointcloud_image.get_height_pixels();
+  point_cloud->height = pointcloud_image.get_height_pixels();
+  point_cloud->width = pointcloud_image.get_width_pixels();
   point_cloud->is_dense = false;
   point_cloud->is_bigendian = false;
 
@@ -694,9 +693,8 @@ k4a_result_t K4AROSDevice::fillColorPointCloud(const k4a::image& pointcloud_imag
   pcd_modifier.resize(point_count);
 
   // Restore actual dimensions as pcd_modifier.resize(n) sets the cloud size to n x 1
-  point_cloud->height = pointcloud_image.get_width_pixels();
-  point_cloud->width = pointcloud_image.get_height_pixels();
-  point_cloud->row_step = pointcloud_image.get_height_pixels() * point_cloud->point_step;
+  point_cloud->height = pointcloud_image.get_height_pixels();
+  point_cloud->width = pointcloud_image.get_width_pixels();
 
   const int16_t* point_cloud_buffer = reinterpret_cast<const int16_t*>(pointcloud_image.get_buffer());
   const uint8_t* color_buffer = color_image.get_buffer();
@@ -730,9 +728,8 @@ k4a_result_t K4AROSDevice::fillColorPointCloud(const k4a::image& pointcloud_imag
 
 k4a_result_t K4AROSDevice::fillPointCloud(const k4a::image& pointcloud_image, sensor_msgs::PointCloud2Ptr& point_cloud)
 {
-  // Flip height and width to match expected memory layout
-  point_cloud->height = pointcloud_image.get_width_pixels();
-  point_cloud->width = pointcloud_image.get_height_pixels();
+  point_cloud->height = pointcloud_image.get_height_pixels();
+  point_cloud->width = pointcloud_image.get_width_pixels();
   point_cloud->is_dense = false;
   point_cloud->is_bigendian = false;
 
@@ -748,9 +745,8 @@ k4a_result_t K4AROSDevice::fillPointCloud(const k4a::image& pointcloud_image, se
   pcd_modifier.resize(point_count);
 
   // Restore actual dimensions as pcd_modifier.resize(n) sets the cloud size to n x 1
-  point_cloud->height = pointcloud_image.get_width_pixels();
-  point_cloud->width = pointcloud_image.get_height_pixels();
-  point_cloud->row_step = pointcloud_image.get_height_pixels() * point_cloud->point_step;
+  point_cloud->height = pointcloud_image.get_height_pixels();
+  point_cloud->width = pointcloud_image.get_width_pixels();
 
   const int16_t* point_cloud_buffer = reinterpret_cast<const int16_t*>(pointcloud_image.get_buffer());
 
